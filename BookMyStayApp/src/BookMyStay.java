c2062e4e5f66a39c3bff12e28179cd8e571e1767
Netraveejay <netraveejay2006@gmail.com>
2026-03-16 09:14:29 +0530

    UC1: Application Entry & Welcome Message

---
+++ BookMyStayApp/src/BookMyStay.java
@@ -0,0 +1,10 @@
+
+public class BookMyStay {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the hotel booking management system");
+        System.out.println("System Initialized Successfully.");
+
+    }
+}
